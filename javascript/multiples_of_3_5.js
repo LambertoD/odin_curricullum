5211eef7a8edf92f2930e1abec90f2baa2ad6b4e
Lamberto C. Diwa <lambertodiwajr@gmail.com>
2014-02-27 12:29:35 -0600

    add directories and project euler image

--- javascript/multiples_of_3_5.js
+++ javascript/multiples_of_3_5.js
@@ -5,9 +5,12 @@ var divisible_by_5_3 = function (number) {
      
 };
 
-var sum = 0;
-
-for (var i = 1; i < 10; i++) {
+var sum_of_multiples_of_3_5 = function (number) {
+  var sum = 0;
+  for (var i = 1; i < number; i++) {
     sum += divisible_by_5_3(i);
-}
+  }	
+  return sum;
+};
+
 alert("Sum is: " + sum);
