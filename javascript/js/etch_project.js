455a90e97151aa690f5e75e2148f53ceced48ac9
Lamberto C. Diwa <lambertodiwajr@gmail.com>
2014-03-05 18:52:19 -0600

    update with dynamic grid arguments

--- javascript/js/etch_project.js
+++ javascript/js/etch_project.js
@@ -1,33 +1,50 @@
 $(function() {
-  var grid_color = $("#color_selected").prop("value");
 
+  $("#clear_grid").fadeOut("fast");
   $("#clear_grid").on("click", function() {
     $("span").css("background", "#eee");
   });
 
-  $("#color_selected").on("click", function() {
-    var grid_color = $("#color_selected").prop("value");
+  $("#generate_grid").on("click", function() {
+    create_grid();
   });
-
-  createGrid16(grid_color);
 
 });
 
+var create_grid = function() {
+  if($("#my_grid").hasClass("showing_in_page")) {
+    $("#my_grid").empty();
+  }
+  var grid_color = $("#color_selected").prop("value");
+  var grid_size = $("#grid_size").prop("value");
+  create_page_grid(grid_size, grid_color);
+  $("#clear_grid").fadeIn("fast");
 
-var createGrid16 = function(grid_color) {
-  var newContainer = $("<div />", { "id" : "grid16"});
-  for (var i = 0; i < 16; i++) {
+}
+
+var create_page_grid = function(size, grid_color) {
+  var side_value = 640 / size;
+  var side = side_value + "px"
+  var newContainer = $("<div />", { "id" : "my_grid", "class": "showing_in_page"});
+  $("<p>Your Etch-A-Sketch Grid</p>").appendTo(newContainer);  
+  $("<p>Start Drawing</p>").appendTo(newContainer);
+  for (var i = 0; i < size; i++) {
     var newDiv = $("<div />", {
-      "class": "cell_row"
+      "class": "cell_row",
+      "class": "clear"
     });
-    for (var j = 0; j < 16; j++) {
+    for (var j = 0; j < size; j++) {
       var newSpan = $("<span />", {
-        "class": "box"}).appendTo(newDiv);    
+        "class": "box",
+        "height": side,
+        "width" : side
+      }).appendTo(newDiv);    
     };
     newDiv.appendTo(newContainer);
   }
   newContainer.appendTo("body");
   $("span").on("mouseenter mouseleave", function() {
     $(this).css("background", grid_color);
-  });
+  });  
+
 }
