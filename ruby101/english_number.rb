c286d5e91b9844f36882efb3c55b388471cf212b
Lamberto C. Diwa <lambertodiwajr@gmail.com>
2014-03-12 18:40:36 -0500

    update english_number to handle thousands and millions

--- ruby101/english_number.rb
+++ ruby101/english_number.rb
@@ -1,27 +1,32 @@
 
 require 'minitest/autorun'
 
-# class TestEntries < Minitest::Unit::TestCase
-#   def setup
-#     @english_numberer = EnglishNumber.new
-#     @number = 0
-#     while (true)
-#       puts "Please enter a number between 1 and 999"
-#       @number = gets.chomp.to_i
-#       unless @number < 1 && @number > 999
-#         break
-#       end
-#     end
-#     puts @english_numberer.english_number @number
-#   end
-#   def test_say_111
-#     assert_match /one hundred eleven/, @english_numberer.english_number(111)
-#   end
-#   def test_say_923
-#     assert_match /nine hundred twenty-three/, @english_numberer.english_number(923)
-    
-#   end
-# end
+class TestEntries < Minitest::Unit::TestCase
+  def setup
+    @english_numberer = EnglishNumber.new
+    # @number = 0
+    # while (true)
+    #   puts "Please enter a number between 1 and 999"
+    #   @number = gets.chomp.to_i
+    #   unless @number < 1 && @number > 999
+    #     break
+    #   end
+    # end
+    # puts @english_numberer.english_number @number
+  end
+  def test_say_111
+    assert_match /one hundred eleven/, @english_numberer.english_number(111)
+  end
+  def test_say_923
+    assert_match /nine hundred twenty-three/, @english_numberer.english_number(923)    
+  end
+  def test_say_1111
+    assert_match /one thousand one hundred eleven/, @english_numberer.english_number(1111)
+  end
+  def test_say_9999
+    puts @english_numberer.english_number(111111111)
+  end
+end
 
 class EnglishNumber
 
@@ -32,6 +37,28 @@ class EnglishNumber
     teenagers = %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
 
     left = number
+    write = left / 1000000
+    left = left - write * 1000000
+
+    if write > 0
+      millions = english_number write
+      num_string += millions + ' million'
+      if left > 0
+        num_string += ' '
+      end
+    end
+
+    write = left / 1000
+    left = left - write*1000
+
+    if write > 0
+      thousands = english_number write
+      num_string = num_string + thousands + ' thousand'
+      if left > 0
+        num_string = num_string + ' '
+      end
+    end
+
     write = left / 100       # get the hundreds to write out
     left = left - write*100  # subtract the hundreds and this is number left
 
