3d4e257c2ca381ca8076277e13688bbed86ea7ec
Lamberto C. Diwa <lambertodiwajr@gmail.com>
2014-03-27 17:08:37 -0500

    add on bot functionality

--- ruby101/bot/bot.rb
+++ ruby101/bot/bot.rb
@@ -14,11 +14,56 @@ class Bot
   end
 
   def greeting
-    @data[:responses][:greeting][rand(@data[:responses][:greeting].length)]
+    random_response :greeting
   end
   def farewell
-    @data[:responses][:farewell][rand(@data[:responses][:farewell].length)]
+    random_response :farewell
   end
 
+  def response_to(input)
+    prepared_input = preprocess(input).downcase
+    sentence = best_sentence(prepared_input)
+    responses = possible_responses(sentence)
+    responses[rand(responses.length)]
+  end
 
+  private
+    def random_response(key)
+      random_index = rand(@data[:responses][key].length)
+      @data[:responses][key][random_index].gsub(/\[name\]/,@name)
+      
+    end
+
+    def preprocess(input)
+      perform_substitutions input
+    end
+
+    def perform_substitutions(input)
+      @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
+      input
+    end
+
+    def best_sentence(input)
+      hot_words = @data[:responses].keys.select do |k|
+        k.class == String && k =~ /^\w+$/
+      end
+      WordPlay.best_sentence(input.sentences, hot_words)
+    end
+
+    def possible_responses(sentence)
+      responses = []
+      @data[:responses].keys.each do |pattern|
+        next unless pattern.is_a?(String)
+        if sentence.match('\b' + pattern.gsub(/\*/, '') + '\b')
+          if pattern.include?('*')
+            responses << @data[:responses][pattern].collect do |phrase|
+              matching_section = sentence.sub(/^.*#{pattern}\s+/, '')
+              phrase.sub('*', WordPlay.switch_pronouns(matching_section))
+            end
+          end
+        end
+      end
+      responses << @data[:responses][:default] if responses.empty?
+      responses.flatten
+    end
 end
